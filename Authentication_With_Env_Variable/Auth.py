83aa0ec06f32cdbc5c9b55c72e6d786840beb290
AKD <contact.mousom@gmail.com>
2023-12-20 00:50:03 +0600

    All steps about how can import all packages and how to execute the dotenv method

---
+++ Authentication_With_Env_Variable/Auth.py
@@ -0,0 +1,12 @@
+'''
+* First of all you have to import os otherwise you can't able to import env variable
+* Now you have to import load_dotenv from dotenv otherwise you can't access the .env file
+* 3rd step you have to execute the load_dontenv() method this method help us to get access our env file
+'''
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
